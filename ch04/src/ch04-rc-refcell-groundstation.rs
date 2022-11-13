bce4cd13d1d56db04331f2f008675cdd92e41a48
iamminji <kimminji.dev@gmail.com>
2022-11-13 21:36:47 +0900

    Study rust-in-action

--- ch04/src/ch04-rc-refcell-groundstation.rs
+++ ch04/src/ch04-rc-refcell-groundstation.rs
@@ -12,4 +12,21 @@ fn main() {
                 radio_freq: 87.65
             }
     ));
+
+    println!("base: {:?}", base);
+
+    {
+        /// base를 가변적으로 대여할 수 있는 새로운 범위를 도입한다.
+        let mut base_2 = base.borrow_mut();
+        base_2.radio_freq -= 12.34;
+        println!("base_2: {:?}", base_2);
+    }
+
+    println!("base: {:?}", base);
+    let mut base_3 = base.borrow_mut();
+    base_3.radio_freq += 43.21;
+
+    println!("base: {:?}", base);
+    println!("base_3: {:?}", base_3);
+
 }
